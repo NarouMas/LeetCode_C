33077ec7cfe41df70112f840f15e63a22a11c40f
NarouMas <ssj6rmp4u4ss@gmail.com>
2023-06-06 22:23:26 +0800

    rearrange and 70, 303

---
+++ DP/0070_Climbing_Stairs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n){
+        int arr[45];
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+        return arr[n];
+    }
+};
