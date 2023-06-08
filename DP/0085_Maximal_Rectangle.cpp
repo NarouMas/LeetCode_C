ff80ed4ac3bf69ac05d10bd339559c5120b19ff0
NarouMas <ssj6rmp4u4ss@gmail.com>
2023-06-08 10:37:18 +0800

    update 85

--- DP/0085_Maximal_Rectangle.cpp
+++ DP/0085_Maximal_Rectangle.cpp
@@ -5,25 +5,29 @@ using namespace std;
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size()));
-        int ans = 0;
-        for(int i = 0; i < matrix.size(); i++)
-            for(int j = 0; j < matrix[0].size(); j++)
-                dp[i][j] = (matrix[i][j] == '0') ? 0 : (j == 0 ? 1 : dp[i][j - 1] + 1);
-
-        for(int i = 0; i < matrix.size(); i++)
+        int r = matrix.size(), c = matrix[0].size(), ans = 0;
+        vector<int> height(c, 0), left(c, 0), right(c, c);
+        for(int i = 0; i < r; i++)
         {
-            for(int j = 0; j < matrix[0].size(); j++)
+            int curLeft = 0, curRight = c;
+            for(int j = 0; j < c; j++)
+                height[j] = matrix[i][j] == '1' ? height[j] + 1 : 0;
+            for(int j = 0; j < c; j++)
             {
-                int len = INT_MAX;
-                for(int k = i; k < matrix.size(); k++)
-                {
-                    if(matrix[k][j] == 0)
-                        break;
-                    len = min(len, dp[k][j]);
-                    ans = max(ans, len * (k - i + 1));
-                }
+                if(matrix[i][j] == '1')
+                    left[j] = max(left[j], curLeft);
+                else
+                    left[j] = 0, curLeft = j + 1;
             }
+            for(int j = c - 1; j >= 0; j--)
+            {
+                if(matrix[i][j] == '1')
+                    right[j] = min(right[j], curRight);
+                else
+                    right[j] = c, curRight = j;
+            }
+            for(int j = 0; j < c; j++)
+                ans = max(height[j] * (right[j] - left[j]), ans);
         }
         return ans;
     }
