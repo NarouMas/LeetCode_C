c843a81f7dc9cca3a13f6a019c72e81f6d7d4aaf
NarouMas <ssj6rmp4u4ss@gmail.com>
2023-11-22 17:03:16 +0800

    167

---
+++ TwoPointers/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        vector<int> res(2, 0);
+        while(i < j)
+        {
+            if(numbers[i] + numbers[j] < target)
+                i++;
+            else if(numbers[i] + numbers[j] > target)
+                j--;
+            else
+            {
+                res[0] = i + 1;
+                res[1] = j + 1;
+                return res;
+            }
+        }
+        return res;
+    }
+};
