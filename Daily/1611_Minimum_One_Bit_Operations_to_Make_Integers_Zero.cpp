3e6ce7dae3e5969739489214b08af423741a68d5
NarouMas <ssj6rmp4u4ss@gmail.com>
2023-11-30 17:36:11 +0800

    1611

---
+++ Daily/1611_Minimum_One_Bit_Operations_to_Make_Integers_Zero.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<unordered_map>
+#include<queue>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n <= 1)
+            return n;
+        int bit = 0;
+        while((1 << bit) <= n)
+            bit++;
+        return ((1 << bit) - 1) - minimumOneBitOperations(n - (1 << (bit - 1)));
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    //for(int i = 1, j = 1; i < 1025; i*=2, j++)
+    //    cout<<i<<":"<<sol.minimumOneBitOperations(i)<<"\n";
+    cout<<sol.minimumOneBitOperations(3)<<"\n";
+    cout<<sol.minimumOneBitOperations(6)<<"\n";
+    cout<<sol.minimumOneBitOperations(8)<<"\n";
+    cout<<sol.minimumOneBitOperations(372544)<<"\n";
+}
