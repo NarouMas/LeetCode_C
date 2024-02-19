2d5319cc32f78b41a4477c87efe0af5481d7e7b2
NarouMas <ssj6rmp4u4ss@gmail.com>
2024-02-19 10:09:15 +0800

    231,1642,2402

---
+++ Daily/2024/Feb/0231_Power_of_Two.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        while(n > 1)
+        {
+            if(n % 2 == 1)
+                return false;
+            n = n >> 1;
+        }
+        return true;
+    }
+};
